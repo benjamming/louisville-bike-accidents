8256f0e57aeae14a93ec9bfa888e2d0f0af8311d
Ben Campbell <ben.campbell.w@gmail.com>
2023-11-06 00:10:44 -0500

    finishing up work for the night

--- code/cleaning_LOJIC.py
+++ code/cleaning_LOJIC.py
@@ -12,17 +12,16 @@ DATA_IN = "data/raw/Louisville_Metro_KY_-_Traffic_Fatalities_and_Suspected_Serio
 DATA_OUT = "data/clean/LOJIC_cycling_data.csv"
 
 
-# Columns to drop
-dropping = ["X", "Y", "NAME", "AGE", "GENDER", "LINK"]
-def drop_unused_columns(df:pd.DataFrame, columns:list) -> pd.DataFrame:
-    return df.drop(columns, axis=1)
 
+# Drop unneeded columns
+# Select only rows which report bicycle crashes
 
-# Rows to drop
-# Selecting only bicycle data, then dropping MODE column
-def select_mode_BICYCLE(df:pd.DataFrame) -> pd.DataFrame:
-    out = df[df['MODE'] == "BICYCLE"]
-    return out.drop("MODE", axis=1)
+def drop_rows_and_columns(df:pd.DataFrame):
+    df = df.drop(["X", "Y", "NAME", "AGE", "GENDER", "LINK",
+                  'RampFromRdwyId', 'RampToRdwyId'], axis=1)
+    # Remove unneeded columns
+    df = df[(df['MODE'] == "BICYCLE") | df["DirAnalysisCode"].str.contains("BICY") == True]
+    return df
 
 
 # Columns to rename
@@ -59,18 +58,51 @@ renames = {'IncidentID': "incident_id",
             'MannerofCollision' : 'manner_of_collision', 
             'RdwyCharacter' : 'roadway_character', 
             'LightCondition' : "light_condition",
-            'RampFromRdwyId' : "ramp_from_roadway_id", 
-            'RampToRdwyId' : "ramp_to_roadway_id", 
             'IsSecondaryCollision' : "secondary_collision", 
-            'ObjectId' : "object_id"}
+            'ObjectId' : "object_id",
+            "MODE":"mode",
+            "SEVERITY" : "severity"}
 
 def rename_columns(df:pd.DataFrame, renames:dict) -> pd.DataFrame:
     return df.rename(renames, axis=1)
 
+
+# SEVERITY column
+def expand_severity_column(df:pd.DataFrame) -> pd.DataFrame:
+    severity = df['SEVERITY']
+    fatalities = severity.apply(lambda x:(x == "FATALITY"))
+    injuries = severity.apply(lambda x:(x == "SUSPECTED SERIOUS INJURY"))
+
+    fatalities.name = "fatality_indicator"
+    injuries.name = "injury_indicator"
+    return pd.concat((df, fatalities, injuries), axis=1)
+
+# Fix time date mess
+
+# Define some parsing expressions
+integer = pyparsing.Word(pyparsing.nums).set_name("integer")
+# Define integer word: a string of numeric characters 0-9
+
+date_expr = integer("year") + '/' + integer("month") + '/' + integer('day')
+time_expr_HHMM = integer("hour") + ":" + integer("minute")
+time_expr_seconds = pyparsing.Literal(":") + integer("second") + "+" + integer("ms")
+
+CollisionDate_expr = date_expr + time_expr_HHMM + time_expr_seconds.suppress()
+
+def fix_CollisionDate(string):
+    CD = CollisionDate_expr.parse_string(string).as_dict()
+    CD = {key:int(value) for key, value in CD}
+    timestamp =  pd.Timestamp(**CollisionDate_expr.parse_string(string).as_dict())
+    return timestamp.tz_localize("UTC").tz_convert("US/Eastern")
+
+
+
+
 def clean(df:pd.DataFrame) -> pd.DataFrame:
-    df = drop_unused_columns(df, dropping)
-    df = select_mode_BICYCLE(df)
-    df = rename_columns(df, renames)
+    df = drop_rows_and_columns(df)
+    df = expand_severity_column(df)
+
+    df = rename_columns(df, renames) # Do this last. Too annoying to deal with before.
 
     return df
 
