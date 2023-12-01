4a7729d2135c763f84fa7391226af42cd9e9cc3a
Ben Campbell <ben.campbell.w@gmail.com>
2023-11-30 23:36:19 -0500

    fixing relative paths

--- code/02_cleaning/02_merge_accident_data.py
+++ code/02_cleaning/02_merge_accident_data.py
@@ -15,10 +15,10 @@ assert path.exists(path_to_cycling_safety_cleaned)
 assert path.exists(path_to_LOJIC_cleaned)
 
 # Intersections with lighted traffic signals.
-path_to_signalized_intersections = "data/raw/Jefferson_County_KY_Signalized_Intersections.csv"
+path_to_signalized_intersections = "../../data/raw/Jefferson_County_KY_Signalized_Intersections.csv"
 
 # Path to write CSV for combined data.
-CSV_OUT = "data/clean/bike_accidents.csv"
+CSV_OUT = "../../data/clean/bike_accidents.csv"
 
 
 def merge_accident_data() -> pd.DataFrame:
